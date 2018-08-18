b67a91ca22c2574c90081aef058cd50488eacef0
Aryan <aryan_02@outlook.com>
2018-08-18 19:46:29 +0530

    Removed "Quietflow.js"

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -11,11 +11,11 @@ $(function () {
     });
   });
 
-  $("body").quietflow({
+/*  $("body").quietflow({
     theme : "bouncingBalls",
     specificColors : [
       "rgba(255, 10, 50, .5)",
       "rgba(10, 255, 50, .5)", 
       "rgba(10, 50, 255, .5)"
     ]
-  })
+  })*/
