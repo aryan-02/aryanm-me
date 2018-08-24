6efff21df58abacec8c559cc80c562db1dfbbce7
Aryan <aryan_02@outlook.com>
2018-08-25 04:06:20 +0530

    Tasty Hamburger

---
+++ public/scripts/base.js
@@ -0,0 +1,12 @@
+  $(document).ready(function(){
+  $('#nav-icon').click(function(){
+    $(this).toggleClass('animate-icon');
+    $('#overlay').fadeToggle();
+  });
+});
+  $(document).ready(function(){
+  $('#overlay').click(function(){
+    $('#nav-icon').removeClass('animate-icon');
+    $('#overlay').toggle();
+  });
+});
--- public/scripts/home.js
+++ public/scripts/home.js
@@ -11,6 +11,7 @@ $(function () {
     });
   });
 
+
 /*  $("body").quietflow({
     theme : "bouncingBalls",
     specificColors : [
