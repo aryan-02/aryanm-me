f03765f5e51ecb2c669162904bfe5a64801d8d4d
Aryan <aryan_02@outlook.com>
2018-09-03 20:06:23 +0530

    Loading Animation

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -1,12 +1,28 @@
-  $(document).ready(function(){
-  $('#nav-icon').click(function(){
-    $(this).toggleClass('animate-icon');
-    $('#overlay').fadeToggle();
-  });
+$(window).on("load",function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");;
+}); 
+
+$("nav a").click(function(e)
+{
+	e.preventDefault();
+	let anchor = $(this);
+	let href = anchor.attr('href');
+	$("body *").fadeOut(150, function(){
+		$("body").css("background", "black");
+		window.location.href = href;
+	});
 });
-  $(document).ready(function(){
-  $('#overlay').click(function(){
-    $('#nav-icon').removeClass('animate-icon');
-    $('#overlay').toggle();
-  });
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('animate-icon');
+		$('#overlay').fadeToggle();
+	});
+});
+	$(document).ready(function(){
+	$('#overlay').click(function(){
+		$('#nav-icon').removeClass('animate-icon');
+		$('#overlay').toggle();
+	});
 });
