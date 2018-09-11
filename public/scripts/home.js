3b4128cf524cbcb8abc0c231243ced924848ab07
Aryan <aryan_02@outlook.com>
2018-09-11 11:45:31 +0530

    Skrollr

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -10,3 +10,13 @@ $(function () {
       topbar.toggleClass('scrolled-half', $(this).scrollTop() > (topbar.outerHeight()));
     });
   });
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+}else
+{
+$(function(){
+    skrollr.init({
+    forceHeight: false
+  });
+});
+}
