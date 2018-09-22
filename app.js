9ca2647687e750b81d8e79ab61bca7a4d318eaf0
Aryan <aryan_02@outlook.com>
2018-09-23 04:40:15 +0530

    Fixed #14

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(sassMiddleware({
 	src: path.join(__dirname, 'public/sass'),
 	dest: path.join(__dirname, 'public/css'),
 	debug: true,
-	prefix: "/css/css",
 	outputStyle: 'compressed',
 	prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
 }));
