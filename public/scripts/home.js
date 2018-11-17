c92be6bf496a31a2f5496230d2fdef32df710df7
Aryan Mediratta <aryan_02@outlook.com>
2018-11-17 23:44:39 +0530

    Put achievements on the home page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,21 @@
+function toggleMore() {
+  setTimeout(function(){
+    let dots = document.getElementById("ellipsis");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("read");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }, 300);
+}
+
 $(function () {
     $(document).scroll(function () {
       let topbar = $("header");
@@ -19,3 +37,42 @@ $(function(){
   });
 });
 }
+
+// Ripple
+
+;(function (window, $) {
+  
+  $(function() {
+    
+    
+    $('.ripple').on('click', function (event) {
+      event.preventDefault();
+      
+      var $div = $('<div/>'),
+          btnOffset = $(this).offset(),
+          xPos = event.pageX - btnOffset.left,
+          yPos = event.pageY - btnOffset.top;
+      
+
+      
+      $div.addClass('ripple-effect');
+      var $ripple = $(".ripple-effect");
+      
+      $ripple.css("height", $(this).height());
+      $ripple.css("width", $(this).height());
+      $div
+        .css({
+          top: yPos - ($ripple.height()/2),
+          left: xPos - ($ripple.width()/2),
+          background: $(this).data("ripple-color")
+        }) 
+        .appendTo($(this));
+
+      window.setTimeout(function(){
+        $div.remove();
+      }, 2000);
+    });
+    
+  });
+  
+})(window, jQuery);
